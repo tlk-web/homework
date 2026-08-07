274854473685435f3ba83554665058a437ab3681
tlk-web <mactransc0mpany@gmail.com>
2026-08-07 23:21:04 +0300

    Add files via upload

--- Leader/js/index.js
+++ Leader/js/index.js
@@ -11,18 +11,18 @@ try {
     {
       BURGER: "burger",
       BURGER_OPEN: "burger--open",
-      HEADER_MENU: "header__menu",
-      HEADER_MENU_OPEN: "header__menu--open",
+      HEADER_MENU: "menu",
+      HEADER_MENU_OPEN: "menu--open",
       lABEL: {
         OPEN: "Открыть меню",
         CLOSE: "Закрыть меню",
       },
       PAGE_BODY: "page__body",
       PAGE_BODY_NO_SCROLL: "page__body--no-scroll",
-      MENU_LINK: "menu__link",
-      BREAKPOINT: 768,
+      MENU_LINK: "menu__item-link",
+      BREAKPOINT: 992,
     },
-    headerFixed
+    headerFixed,
   );
 } catch (error) {
   console.error(error);

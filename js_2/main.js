ee82e3786573828115682e270fdbcb8573830c98
tlk-web <mactransc0mpany@gmail.com>
2026-08-16 19:38:15 +0300

    Add files via upload

---
+++ js_2/main.js
@@ -0,0 +1,78 @@
+// Задача 1
+// let number = +prompt("Введите число");
+
+// if (number % 2 === 0) {
+//   alert("Число чётное");
+// } else {
+//   alert("Число нечёт");
+// }
+
+// Задача 2
+// const age = 777;
+
+// // const discount = age < 18 ? "10%" : age >= 18 && age <= 65 ? "20%" : "30%";
+
+// // console.log("Скидка " + discount);
+
+// // *Дополнительное задание
+// switch (true) {
+//   case age < 18:
+//     discount = "Скидка 10%";
+//     break;
+//   case age >= 18 && age <= 65:
+//     discount = "Скидка 20%";
+//     break;
+//   case age >= 65 && age <= 95:
+//     discount = "Скидка 30%";
+//     break;
+//   default:
+//     discount = "Ты что вампир?!";
+// }
+// console.log(discount);
+
+// Задача 3
+
+// let username = prompt("Имя пользователя");
+// let password = prompt("Пароль");
+
+// if ((username === "admin" || "user") && password === "123456") {
+//   alert("Доступ разрешён");
+// } else {
+//   alert("Доступ запрещён");
+// }
+
+// Задача 4
+
+// let weight = +prompt("Введите вес поссылки в киллограммах");
+// let deliveryType = prompt("Введите тип доставки (Стандарт, Экспресс, Премиум)");
+
+// let summa;
+// if (weight <= 0) {
+//   alert("Некорректный вес посылки");
+// } else if (weight < 1) {
+//   summa = 5;
+// } else if (weight <= 5) {
+//   summa = 10;
+// } else if (weight > 5) {
+//   summa = 15;
+// }
+
+// switch (deliveryType) {
+//   case "Стандарт":
+//     factor = 1;
+//     break;
+//   case "Экспресс":
+//     factor = 1.5;
+//     break;
+//   case "Премиум":
+//     factor = 2;
+//     break;
+//   default:
+//     factor = "Неверный тип доставки";
+// }
+
+// price = summa * factor;
+
+// alert(price);
+
+//В любом случае я попробовал Хах)))))))))))))

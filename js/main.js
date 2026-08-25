6247572d77b1d80440807548f09ba84a23b10fa3
tlk-web <mactransc0mpany@gmail.com>
2026-08-25 15:13:19 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -15,16 +15,15 @@
 // Задача 2.
 // Создайте функцию isEmpty, которая проверяет является ли переданный объект пустым. Если объект пуст - верните true, в противном случае false.
 
-// const trueFalse = {};
-
-// function isEmpty() {
-//   for (let key in trueFalse) {
+// function isEmpty(obj) {
+//   for (let key in obj) {
 //     return false;
 //   }
 //   return true;
 // }
 
-// console.log(isEmpty());
+// console.log(isEmpty({})); // true
+// console.log(isEmpty({ a: 1 })); // false
 
 // Задача 3.
 // Создайте объект task с несколькими свойствами: title, description, isCompleted.
@@ -45,26 +44,27 @@
 // const updateTask = cloneAndModify(task, { isCompleted: true, title: "Готово" });
 
 // // И вот конечная переменная с изменением modifications чтобы оставить только значения ключей можно убрать вложенность (key, updatetask[key]) > console.log(updateTaskp[key])
-// for (const key in task) {
+// for (const key in updateTask) {
 //   console.log(key, updateTask[key]);
 // }
 
 // Задача 4.
 // Создайте функцию callAllMethods, которая принимает объект и вызывает все его методы.
-function callAllMethods(obj) {
-  for (const key in obj) {
-    obj[key]();
-  }
-}
+// function callAllMethods(obj) {
+//   for (const key in obj) {
+//     if (typeof obj[key] === "function") {
+//       obj[key]();
+//     }
+//   }
+// }
 
-const myObject = {
-  method1() {
-    console.log("Метод 1 вызван");
-  },
-  method2() {
-    console.log("Метод 2 вызван");
-  },
-};
+// const myObject = {
+//   method1() {
+//     console.log("Метод 1 вызван");
+//   },
+//   method2() {
+//     console.log("Метод 2 вызван");
+//   },
+// };
 
-// Че за фигня как он показывать начал хахахахаха без консоль лог
-callAllMethods(myObject);
+// callAllMethods(myObject);
